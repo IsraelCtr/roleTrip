ea32d628f01a21f4894e9fd59aebd57cbcb08b02
Israel Castro <94873358+IsraelCtr@users.noreply.github.com>
2025-06-14 15:25:07 -0300

    Update vite.config.js

--- role/frontRoleTrip/frontRoleTrip/vite.config.js
+++ role/frontRoleTrip/frontRoleTrip/vite.config.js
@@ -1,9 +1,6 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  // eslint-disable-next-line no-undef
-  base: process.env.VITE_BASE || '/',
+  build: {
+    outDir: 'dist'
+  }
 })
